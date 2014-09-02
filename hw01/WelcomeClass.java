b1557eacc62f834644f1ba168a80b45313e120ac
Phebe de Guzman <pld217@lehigh.edu>
2014-09-02 21:44:44 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,42 @@
+////////////////////////////////////////////
+//Phebe de Guzman 
+//hw01 
+//WecomeClass
+//
+//  first compile the program 
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the following
+    System.out.println("    -------------");
+    System.out.println("    |  WELCOME  |");
+    System.out.println("    -------------");
+    System.out.println("   ^  ^  ^  ^  ^  ^");
+    System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("  |-P--L--D--2--1--7-|");
+    System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("    v  v  v  v  v  v");
+    System.out.println("When I was in kindergarden I decided to drop the o in my name, Phoebe.");
+    System.out.println("So now I spell it Phebe, but all my legal documents including my Lehigh identification include the o");
+    System.out.println("which makes for very confused professors.");
+
+    
+    //test/testtest
+    }
+}
+
+    
+
+   
+  
+
+  
+    
+
